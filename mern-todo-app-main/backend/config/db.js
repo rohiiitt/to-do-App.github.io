90a955e7a7dc8f80130d7a2f66833acfa214819c
Rohit kumar <rk9870359833@gmail.com>
2025-06-30 11:46:47 +0530

    to-do App

---
+++ mern-todo-app-main/backend/config/db.js
@@ -0,0 +1,27 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
+
+
+
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
+};
+
+export default connectDB;
+
